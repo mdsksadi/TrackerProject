daba40bc8df3039185f880eb6eaa17cf4160f272
mdsksadi <sadiewu12345@gmail.com>
2023-08-30 23:23:34 +0200

    Uncommented clean code

--- include/tracker.hpp
+++ include/tracker.hpp
@@ -8,15 +8,11 @@
 class TrackerProject
 {
 public:
-    // Constructor
     TrackerProject();
-    // Distractor
     ~TrackerProject();
 
-    // Function for capture frame with camera
     int8_t init();
 
-    // Boolean true as active
     bool active{true};
 };
 
